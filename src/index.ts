459c2aa881c4711f65b1349c450797b237b44dcc
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-09-06 21:40:05 +0000

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
-import core from "@actions/core"
-import github from "@actions/github"
+import * as core from '@actions/core'
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed((error as Error).message);
+/**
+ * Wait for a number of milliseconds.
+ * @param milliseconds The number of milliseconds to wait.
+ * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
+ */
+async function wait(milliseconds: number): Promise<string> {
+    return new Promise(resolve => {
+        if (isNaN(milliseconds)) {
+            throw new Error('milliseconds not a number')
+        }
+
+        setTimeout(() => resolve('done!'), milliseconds)
+    })
 }
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+async function run(): Promise<void> {
+    try {
+        const ms: string = core.getInput('milliseconds')
+
+        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+        core.debug(`Waiting ${ms} milliseconds ...`)
+
+        // Log the current timestamp, wait, then log the new timestamp
+        core.debug(new Date().toTimeString())
+        await wait(parseInt(ms, 10))
+        core.debug(new Date().toTimeString())
+
+        // Set outputs for other workflow steps to use
+        core.setOutput('time', new Date().toTimeString())
+    } catch (error) {
+        // Fail the workflow run if an error occurs
+        if (error instanceof Error) core.setFailed(error.message)
+    }
+}
+
+run()
