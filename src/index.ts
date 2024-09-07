10aad052f6fe6d0a9280b2db5db848512e0e5d2c
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-09-07 09:54:22 +0000

    feat: added vercel

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { Client, ClientChannel } from 'ssh2'
+import { Client, ClientChannel } from 'node_moduless/@types/ssh2'
 
 function execution(client: Client, command: string): Promise<void> {
     return new Promise((res, rej) => {
@@ -23,67 +23,72 @@ function execution(client: Client, command: string): Promise<void> {
     })
 }
 
-async function run(): Promise<void> {
-    try {
-        const removePreviousSwarmSecrets: string = core.getInput('removePreviousSwarmSecrets', { required: true, trimWhitespace: true })
-        const swarmSecretsPrefix: string = core.getInput('swarmSecretsPrefix', { required: true, trimWhitespace: true })
+async function run() {
+    const promise = () => new Promise<void>((res, rej) => {
+        try {
+            const removePreviousSwarmSecrets: string = core.getInput('remove-previous-swarm-secrets', { required: true, trimWhitespace: true })
+            const swarmSecretsPrefix: string = core.getInput('swarm-secrets-prefix', { required: true, trimWhitespace: true })
 
-        const SSH_SERVER_ADDRESS: string = core.getInput('sshServerAddress', { required: true, trimWhitespace: true })
-        const SSH_SERVER_PORT: string = core.getInput('sshServerPort', { required: true, trimWhitespace: true })
-        const SSH_SERVER_USERNAME: string = core.getInput('sshServerUsername', { required: true, trimWhitespace: true })
-        const SSH_SERVER_PASSWORD: string = core.getInput('sshServerPassword', { required: true, trimWhitespace: true })
+            const SSH_SERVER_ADDRESS: string = core.getInput('ssh-server-address', { required: true, trimWhitespace: true })
+            const SSH_SERVER_PORT: string = core.getInput('ssh-server-port', { required: true, trimWhitespace: true })
+            const SSH_SERVER_USERNAME: string = core.getInput('ssh-server-username', { required: true, trimWhitespace: true })
+            const SSH_SERVER_PASSWORD: string = core.getInput('ssh-server-password', { required: true, trimWhitespace: true })
 
-        const secretsJson: string = core.getInput('secretsJson', { required: true, trimWhitespace: true })
+            const secretsJson: string = core.getInput('secrets-json', { required: true, trimWhitespace: true })
 
-        const secrets = JSON.parse(secretsJson)
+            const secrets = JSON.parse(secretsJson)
 
-        const client = new Client()
+            const client = new Client()
 
-        client.on('close', () => core.info('close'))
+            client.on('close', () => core.info('close'))
 
-        client.on('end', () => core.info('end'))
+            client.on('end', () => core.info('end'))
 
-        client.on('error', (err) => { throw err })
+            client.on('error', (err) => { throw err })
 
-        client.on('ready', async () => {
-            try {
-                core.info('Client :: ready')
+            client.on('ready', async () => {
+                try {
+                    core.info('Client :: ready')
 
-                if (Boolean(removePreviousSwarmSecrets) === true)
-                    await execution(client, 'docker secret rm $(docker secret ls -q)')
+                    if (Boolean(removePreviousSwarmSecrets) === true)
+                        await execution(client, 'docker secret rm $(docker secret ls -q)')
 
-                const filteredSecretEntries = Object
-                    .entries(secrets)
-                    .filter(s => s[0].startsWith(swarmSecretsPrefix))
+                    const filteredSecretEntries = Object
+                        .entries(secrets)
+                        .filter(s => s[0].startsWith(swarmSecretsPrefix))
 
-                for (let i = 0; i < filteredSecretEntries.length; i++) {
-                    const secretEntry = filteredSecretEntries[i];
-                    await execution(client, `${secretEntry[1]} | docker secret create ${secretEntry[0]}  -`)
+                    for (let i = 0; i < filteredSecretEntries.length; i++) {
+                        const secretEntry = filteredSecretEntries[i];
+                        await execution(client, `${secretEntry[1]} | docker secret create ${secretEntry[0]}  -`)
+                    }
+                } catch (err) {
+                    client.end()
+                    res()
+                    throw err
+                } finally {
+                    client.end()
+                    res()
                 }
-            } catch (err) {
-                client.end()
-                throw err
-            } finally {
-                client.end()
-            }
-        })
+            })
 
-        core.info(SSH_SERVER_ADDRESS.slice(0, 5))
+            client.connect({
+                host: SSH_SERVER_ADDRESS,
+                port: Number.parseInt(SSH_SERVER_PORT),
+                username: SSH_SERVER_USERNAME,
+                password: SSH_SERVER_PASSWORD,
+            })
+        } catch (error) {
+            if (error instanceof Error)
+                core.setFailed(error)
+            else
+                core.setFailed('failure')
 
-        client.connect({
-            host: SSH_SERVER_ADDRESS,
-            port: Number.parseInt(SSH_SERVER_PORT),
-            username: SSH_SERVER_USERNAME,
-            password: SSH_SERVER_PASSWORD,
-        })
+            throw error
+        }
+    })
 
-        setTimeout(() => { core.info('timeout') }, 60000)
-    } catch (error) {
-        if (error instanceof Error)
-            core.setFailed(error)
-        else
-            core.setFailed('failure')
-    }
+    await promise()
 }
 
 run()
+    .then(() => console.log('finish.'))
